579d951b1257d89fdfd453b204583c0161bf126d
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2026-04-21 15:39:05 +0200

    refactor: separate user management into components

---
+++ frontend/src/components/forms/user-forms/UserForms.tsx
@@ -0,0 +1,137 @@
+import ApiContext from "@/context/ApiContext";
+import { ChangeEvent, useContext, useState } from "react";
+import style from './UserForms.module.css';
+import SubmitButton from "@/components/buttons/SubmitButton";
+
+
+interface UpdateUserData {
+  id: string | null;
+  username: string | null;
+  email: string | null;
+}
+
+interface GetDeleteUserData {
+  id: string | null;
+}
+
+interface CreateUserData {
+  username: string | null;
+  email: string | null;
+  password: string | null;
+}
+
+export const UpdateUserByIdForm  = () => {
+  const { updateUserById, loadingApi } = useContext(ApiContext);
+
+  const [data, setData] = useState<UpdateUserData>({
+    id: null,
+    username: null,
+    email: null,
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const sharedProps = {
+    className: "flex-column-relative center w-100 h-100 " + style["input"],
+    onChange: handleChange,
+  };
+
+  return (
+    <>
+      <h4>Update user</h4>
+      <input {...sharedProps} style={{ width: "60%", marginRight: "2%" }} name="id" type="number" placeholder="ID" />
+      <input {...sharedProps} name="username" type="text" placeholder="Username" />
+      <input {...sharedProps} name="email" type="text" placeholder="Email" />
+      <SubmitButton
+        onClick={() => updateUserById(data)}
+        disabled={loadingApi}
+      />
+    </>
+  );
+};
+
+export const GetUserByIdForm = () => {
+  const { getUserById, loadingApi } = useContext(ApiContext);
+
+  const [data, setData] = useState<GetDeleteUserData>({ id: null });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const sharedProps = {
+    className: "flex-column-relative center w-100 h-100 " + style["input"],
+    onChange: handleChange,
+  };
+
+  return (
+    <>
+      <h4>Get user</h4>
+      <input {...sharedProps} name="id" type="number" placeholder="ID" />
+      <SubmitButton
+        onClick={() => getUserById(data.id)}
+        disabled={loadingApi}
+      />
+    </>
+  );
+};
+
+export const DeleteUserByIdForm = () => {
+  const { deleteUserById, loadingApi } = useContext(ApiContext);
+
+  const [data, setData] = useState<GetDeleteUserData>({ id: null });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const sharedProps = {
+    className: "flex-column-relative center w-100 h-100 " + style["input"],
+    onChange: handleChange,
+  };
+
+  return (
+    <>
+      <h4>Delete user</h4>
+      <input {...sharedProps} name="id" type="number" placeholder="ID" />
+      <SubmitButton
+        onClick={() => deleteUserById(data.id)}
+        disabled={loadingApi}
+      />
+    </>
+  );
+};
+
+export const CreateUserForm = () => {
+  const { createUser, loadingApi } = useContext(ApiContext);
+
+  const [data, setData] = useState<CreateUserData>({
+    username: null,
+    email: null,
+    password: null,
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const sharedProps = {
+    className: "flex-column-relative center w-100 h-100 " + style["input"],
+    onChange: handleChange,
+  };
+
+  return (
+    <>
+      <h4>Create user</h4>
+      <input {...sharedProps} name="username" type="text" placeholder="Username" />
+      <input {...sharedProps} name="email" type="text" placeholder="Email" />
+      <input {...sharedProps} name="password" type="password" placeholder="Password" />
+      <SubmitButton
+        onClick={() => createUser(data)}
+        disabled={loadingApi}
+      />
+    </>
+  );
+};
---
+++ frontend/src/components/tables/users-table/UsersTable.tsx
@@ -0,0 +1,48 @@
+import SubmitButton from "@/components/buttons/SubmitButton";
+import ApiContext, { UserDto } from "@/context/ApiContext";
+import { useContext, useState } from "react";
+
+export const UsersTable = () => {
+  const { loadingApi, getAllUsers } = useContext(ApiContext);
+  const [users, setUsers] = useState<UserDto[]>([]);
+
+  const handleClick = async () => {
+    const fetchedUsers = await getAllUsers();
+    setUsers(fetchedUsers);
+  };
+
+  const rowMargin = "2px 5px 2px 0";
+
+  return (
+    <>
+      <h4>Users</h4>
+      <SubmitButton
+        onClick={handleClick}
+        disabled={loadingApi}
+        label="Show usernames"
+      />
+      <div
+        className="flex-column-relative w-100"
+        style={{ height: "200px", overflowY: "auto", backgroundColor: "#212324ff", padding: "0 8px" }}
+      >
+        {users.map((i) => (
+          <div key={i.id} className="flex-row-relative">
+            <div
+              className="flex-column-row shape-soft text-center"
+              style={{ backgroundColor: "#3b3d3eff", minWidth: "28px", padding: "0 5px", margin: rowMargin, color: "#989fa1ff" }}
+            >
+              <span style={{ fontSize: "12px", color: "#585c5eff" }}>#</span>
+              {i.id}
+            </div>
+            <div
+              className="flex-column-row shape-soft text-center"
+              style={{ margin: rowMargin, fontWeight: "800", fontFamily: '"Lilita One", sans-serif', padding: "0 5px", color: "#b3bbbeff", lineHeight: "30px" }}
+            >
+              {i.username}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
