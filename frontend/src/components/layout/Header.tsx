282d55dd30549baf01777fddee7ad40200fd311c
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2026-04-23 19:37:03 +0200

    refactor: make components out of svg icons

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -4,43 +4,7 @@ import { JSX, useState } from "react";
 import { APP_CONSTANTS } from "@/utils/constants/app-constants";
 import Link from "next/link";
 import NavBar from "./NavBar";
-
-interface SVGLogoProps {
-    hovered: boolean;
-}
-
-const SVGLogo = ({ hovered }: SVGLogoProps): JSX.Element => {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlSpace="preserve"
-            className={
-                "flex-column-relative center w-full h-full rounded " +
-                "[fill-rule:evenodd] [clip-rule:evenodd] [stroke-linejoin:round]" +
-                "[stroke-miterlimit:2]"
-            }
-            viewBox="0 0 640 640"
-        >
-            <path
-                d={
-                    "M320 312c-66.3 0-120-53.7-120-120S253.7 72 320 72s120 53.7" +
-                    " 120 120-53.7 120-120 120zm-30.5 56h61c9.7 0 17.5 7.8 " +
-                    "17.5 17.5 0 4.2-1.5 8.2-4.2 11.4l-27.4 32 31 " +
-                    "115.1h.6l34.6-138.5c2.2-8.7 11.1-14 19.5-10.8C484 " +
-                    "418.3 528 478.3 528 548.5c0 15.1-12.3 27.4-27.4 " +
-                    "27.4l-361.2.1c-15.1 0-27.4-12.3-27.4-27.4 0-70.2 " +
-                    "44-130.2 105.9-153.8 8.4-3.2 17.3 2.1 19.5 10.8L272 " +
-                    "544.1h.6l31-115.1-27.4-32c-2.7-3.2-4.2-7.2-4.2-11.4 " +
-                    "0-9.7 7.8-17.5 17.5-17.5z"
-                }
-                className="transition-[fill] duration-150 [fill-rule:nonzero]"
-                style={{
-                    fill: hovered ? "#ffffff" : "#6a6f74ff",
-                }}
-            />
-        </svg>
-    );
-};
+import LogoIcon from "../icons/svg/LogoIcon";
 
 const Logo = (): JSX.Element => {
     const [hovered, setHovered] = useState<boolean>(false);
@@ -66,7 +30,7 @@ const Logo = (): JSX.Element => {
                     height: `${APP_CONSTANTS.DIMENSIONS.HEADER.HEIGHT * 0.65}px`,
                 }}
             >
-                <SVGLogo hovered={hovered} />
+                <LogoIcon hovered={hovered} />
             </div>
             <p
                 className={
--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -2,34 +2,11 @@ import { APP_CONSTANTS } from "@/utils/constants/app-constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { JSX, useState } from "react";
+import HomeIcon from "../icons/svg/HomeIcon";
 
 interface NavLinkProps {
     endPoint: string;
 }
-
-interface SvgHomeProps extends React.SVGProps<SVGSVGElement> {}
-
-const SvgHome = (props: SvgHomeProps): JSX.Element => {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 640 640"
-            {...props}
-        >
-            <path
-                d={
-                    "M341.8 72.6c-12.3-11.4-31.3-11.4-43.5 " +
-                    "0l-224 208c-9.6 9-12.8 22.9-8 35.1C71.1 " +
-                    "327.9 82.8 336 96 336h16v176c0 35.3 28.7 " +
-                    "64 64 64h288c35.3 0 64-28.7 64-64V336h16c13.2 " +
-                    "0 25-8.1 29.8-20.3 4.8-12.2 1.6-26.2-8-35.1l-" +
-                    "224-208zM304 384h32c26.5 0 48 21.5 48 " +
-                    "48v96H256v-96c0-26.5 21.5-48 48-48z"
-                }
-            />
-        </svg>
-    );
-};
 
 const NavLink = ({ endPoint }: NavLinkProps): JSX.Element => {
     const [hovered, setHovered] = useState<boolean>(false);
@@ -54,7 +31,7 @@ const NavLink = ({ endPoint }: NavLinkProps): JSX.Element => {
             >
                 {/* ❕ Capitalize first letter */}
                 {endPoint === "" ? (
-                    <SvgHome
+                    <HomeIcon
                         width="24"
                         height="24"
                         fill={
