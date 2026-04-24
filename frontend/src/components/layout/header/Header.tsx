b4609933c7e0b8c81432525fd2988703b0bb7c2a
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2026-04-24 11:47:42 +0200

    feat: center logo

--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -7,68 +7,68 @@ import NavBar from "./components/NavBar";
 import LogoIcon from "../../icons/svg/LogoIcon";
 
 const Logo = (): JSX.Element => {
-  const [hovered, setHovered] = useState<boolean>(false);
+    const [hovered, setHovered] = useState<boolean>(false);
 
-  return (
-    <Link
-      href="/"
-      rel="noopener noreferrer"
-      className="flex-column-relative center w-[140px]"
-      style={{
-        height: `${APP_CONSTANTS.DIMENSIONS.HEADER.HEIGHT - 5}px`,
-      }}
-      onMouseEnter={() => {
-        setHovered(true);
-      }}
-      onMouseLeave={() => {
-        setHovered(false);
-      }}
-    >
-      <div
-        className={
-          "flex-column-absolute top-0 sm:top-1/2 h-[45%] sm:h-[65%] " +
-          "mt-[0] sm:mt-[3px] sm:-translate-y-1/2"
-        }
-      >
-        <LogoIcon hovered={hovered} />
-      </div>
-      <p
-        className={
-          "flex-column-absolute top-1/2 sm:top-[90%] font-['Lilita_One',sans-serif] " +
-          "transition-[fill] duration-200 text-xs sm:text-l -translate-y-1/2"
-        }
-        style={{
-          color: hovered ? "#ffffff" : "#969fa1ff",
-          transform: `scale(${hovered ? 1.1 : 1})`,
-        }}
-      >
-        Admin
-      </p>
-    </Link>
-  );
+    return (
+        <Link
+            href="/"
+            rel="noopener noreferrer"
+            className="flex-column-relative center w-[140px]"
+            style={{
+                height: `${APP_CONSTANTS.DIMENSIONS.HEADER.HEIGHT - 5}px`,
+            }}
+            onMouseEnter={() => {
+                setHovered(true);
+            }}
+            onMouseLeave={() => {
+                setHovered(false);
+            }}
+        >
+            <div
+                className={
+                    "flex-column-absolute top-0 sm:top-1/2 h-[45%] sm:h-[65%] " +
+                    "mt-[0] sm:mt-[3px] sm:-translate-y-[65%]"
+                }
+            >
+                <LogoIcon hovered={hovered} />
+            </div>
+            <p
+                className={
+                    "flex-column-absolute top-1/2 sm:top-[82%] font-['Lilita_One',sans-serif] " +
+                    "transition-[fill] duration-200 text-xs sm:text-l -translate-y-1/2"
+                }
+                style={{
+                    color: hovered ? "#ffffff" : "#969fa1ff",
+                    transform: `scale(${hovered ? 1.1 : 1})`,
+                }}
+            >
+                Admin
+            </p>
+        </Link>
+    );
 };
 
 const Header = (): JSX.Element => {
-  const height = APP_CONSTANTS.DIMENSIONS.HEADER.HEIGHT;
+    const height = APP_CONSTANTS.DIMENSIONS.HEADER.HEIGHT;
 
-  return (
-    <header
-      className="flex-column-fixed w-full top-0 border-b-4 border-[#34383bff]"
-      style={{
-        height: `${height}px`,
-      }}
-    >
-      <section
-        className={
-          "flex-column-relative items-center justify-center " +
-          "w-full sm:items-start sm:justify-start"
-        }
-      >
-        <Logo />
-        <NavBar />
-      </section>
-    </header>
-  );
+    return (
+        <header
+            className="flex-column-fixed w-full top-0 border-b-4 border-[#34383bff]"
+            style={{
+                height: `${height}px`,
+            }}
+        >
+            <section
+                className={
+                    "flex-column-relative items-center justify-center " +
+                    "w-full sm:items-start sm:justify-start"
+                }
+            >
+                <Logo />
+                <NavBar />
+            </section>
+        </header>
+    );
 };
 
 export default Header;
