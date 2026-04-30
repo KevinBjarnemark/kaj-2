644f83a49970d652fd89fd92a1f5e739c6fb5779
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2026-04-30 10:05:26 +0200

    refactor: move CSS utilities to utility layer

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import "./utilities.css";
 import { APP_CONSTANTS } from "@/utils/constants/app-constants";
 import Header from "@/components/layout/header/Header";
 import ApiProvider from "@/context/ApiProvider";
@@ -7,34 +6,34 @@ import { JSX } from "react";
 import Footer from "@/components/layout/footer/Footer";
 
 interface RootLayoutProps {
-    // ❕ `React.ReactNode` accepts anything that can be rendered
-    children: React.ReactNode;
+  // ❕ `React.ReactNode` accepts anything that can be rendered
+  children: React.ReactNode;
 }
 
 export const metadata = {
-    title: {
-        template: `%s | ${APP_CONSTANTS.APP_NAME}`,
-        default: `${APP_CONSTANTS.APP_NAME}`,
-    },
+  title: {
+    template: `%s | ${APP_CONSTANTS.APP_NAME}`,
+    default: `${APP_CONSTANTS.APP_NAME}`,
+  },
 };
 
 const RootLayout = ({ children }: RootLayoutProps): JSX.Element => {
-    return (
-        <html lang="en">
-            <body className="flex-column-relative center w-full">
-                <Header />
-                <main
-                    className={`
+  return (
+    <html lang="en">
+      <body className="flex-column-relative center w-full">
+        <Header />
+        <main
+          className={`
                         flex-column-relative w-full pb-20 
                         mt-[var(--header-height)]
                     `}
-                >
-                    <ApiProvider>{children}</ApiProvider>
-                </main>
-                <Footer />
-            </body>
-        </html>
-    );
+        >
+          <ApiProvider>{children}</ApiProvider>
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
